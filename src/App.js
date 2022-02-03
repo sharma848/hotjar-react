a6d08850af4d6c3c83259b3edda58eedc537f600
B0227983 <abhishek1.sharma@airtel.com>
2022-02-03 12:49:51 +0530

    home page design added

--- src/App.js
+++ src/App.js
@@ -7,13 +7,19 @@ function App() {
 
   useEffect(() => {
     hotjar.initialize(2812984,6);
+    window.hj('trigger', 'home_page_hotjar_application')
   },[]);
   return (
     <div className="App">
       <header className="App-header">
         <p>
-          Hotjar Application
+          Hotjar Application Tesing App
         </p>
+        <button className="button">Sign In</button>
+        <button className="button sign-up">Sign Up</button>
+        <button className="button subscribe">Let's Begin</button>
+        <button className="button about-us">About Us</button>
+        <button className="button search">Search</button>
       </header>
     </div>
   );
