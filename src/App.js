313d26209331f252cd505cf51a3bc9cff971275a
B0227983 <abhishek1.sharma@airtel.com>
2022-02-03 12:32:04 +0530

    hotjar added and intialized

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import { hotjar } from 'react-hotjar'
+import { useEffect } from 'react';
 
 function App() {
+
+  useEffect(() => {
+    hotjar.initialize(2812984,6);
+  },[]);
   return (
     <div className="App">
       <header className="App-header">
