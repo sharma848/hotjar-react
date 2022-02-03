53df27c8cc3fa4f45c0e72cb68771ebc25173f79
B0227983 <abhishek1.sharma@airtel.com>
2022-02-03 13:59:05 +0530

    details page added

---
+++ src/Details.js
@@ -0,0 +1,18 @@
+import './index.css';
+
+const Details = () => {
+    return <div className="App">
+        <header className="App-header details">
+            <p>
+            Hotjar Application Tesing App details page
+            </p>
+            <button className="button">Sign In</button>
+            <button className="button sign-up">Sign Up</button>
+            <button className="button subscribe">Let's Begin</button>
+            <button className="button about-us">About Us</button>
+            <button className="button search">Search</button>
+        </header>
+    </div>
+};
+
+export default Details;
--- src/index.js
+++ src/index.js
@@ -3,10 +3,17 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Details from './Details';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/details" element={<Details />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
